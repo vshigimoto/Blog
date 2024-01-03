7eeaf556b4bdcd57c3419524461f7f41bc5ff291
Amirkhan <shigimoto@mail.ru>
2024-01-03 21:50:56 +0600

    add server running

--- internal/blog/server/server.go
+++ internal/blog/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/redis/go-redis"
@@ -26,6 +27,27 @@ func New(eng *gin.Engine, cfg *config.Config, db *sqlx.DB, rdc *redis.Client, lo
 	}
 }
 
-func (s *Server) Run() error {
-	
+func (s *Server) Run() {
+	// add context with cancel for gShutdown
+	go func() {
+		s.log.Infof("Server on port %d start", s.cfg.HttpServer.Port)
+		addr := fmt.Sprintf(":%d", s.cfg.HttpServer.Port)
+		err := s.eng.Run(addr)
+		if err != nil {
+			s.log.Infof("Fail run user server %v", err)
+			return
+		}
+	}()
+	go func() {
+		s.log.Infof("Server on port %d start", s.cfg.HttpServer.AdminPort)
+		addr := fmt.Sprintf(":%d", s.cfg.HttpServer.AdminPort)
+		err := s.eng.Run(addr)
+		if err != nil {
+			s.log.Infof("Fail run admin server %v", err)
+			return
+		}
+	}()
+	// add debug server with http.ListenAndServe and pprof. Add pprof in config
+	// add graceful shutdown
+	// init handlers
 }
